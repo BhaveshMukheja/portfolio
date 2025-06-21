e18286588555b423299fc91f3e5e81fce08dbcb2
Bhavesh <122824805+BhaveshMukheja@users.noreply.github.com>
2025-06-21 13:47:50 +0530

    feat: completed terminal and added phone UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import Skill from './Components/Skills/comp'
 import Timeline from './Components/Timeline/comp'
 import Projects from './Components/Projects/comp'
 import Competions from './Components/Competitions/comp'
+import TerminalComponent from './Components/TerminalComponent/comp'
+import Playground from './Components/MLPlayground/comp'
+import IPhoneComp from './Components/AboutMe/comp'
 
 
 export default function Home() {
@@ -18,7 +21,8 @@ export default function Home() {
       <Timeline/>
       <Projects/>
       <Competions/>
-      
+      <TerminalComponent/>
+      <IPhoneComp/>     
     </>
   )
 }
